5a0fbd751c7e1f3824345cd1414cf16cf55cf4d6
ぬー <ryo.091979@gmail.com>
2019-09-11 21:29:53 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 function buildHTML(message){
-  if ( message.image ) {
+  var img = message.image ? `<img src=
+   ${ message.image }>` :"";
     var html =
      `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
@@ -18,26 +19,7 @@ function buildHTML(message){
         <img src=${message.image} >
       </div>`
     return html;
-  } else {
-    var html =
-     `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-    return html;
-  };
-}
+  }
 $('.js-form').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
