c6ccce306e1b3b90c629ab871bafa6eae9c0f3b1
numazaki345 <ryo.091979@gmail.com>
2019-09-14 21:39:57 +0900

    Revert "[WIP]chat-space非同期通信"

--- app/views/messages/create.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.id @message.id
-json.user_name @message.user.name
-json.date datetime.to_s(:datetime)
-json.content @message.content
-json.image @message.image_url
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module ChatSpace3
       g.helper false
       g.test_framework false
       config.i18n.default_locale = :ja
-      config.time_zone = 'Tokyo'
     end
   end
 end
