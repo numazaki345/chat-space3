64de22e65f13ec266ab70269fc9366a4a33b7af1
ぬー <ryo.091979@gmail.com>
2020-01-14 19:58:51 +0900

    console.logの消し忘れ

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -37,7 +37,6 @@ $(function() {
   $(function(){
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
-      console.log(input);
 
       $.ajax({
         type: 'GET',
