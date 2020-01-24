1d136e12d33236044533e6a25ec6e3ed9f4f13e4
ぬー <ryo.091979@gmail.com>
2020-01-24 21:35:45 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'chat-space3'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:<numazaki345>/<chat-space3>.git'
+set :repo_url,  'git@github.com:numazaki345>/<chat-space3.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
