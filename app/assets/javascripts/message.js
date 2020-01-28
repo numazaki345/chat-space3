becf749cba70dbdb024ace74ce0568ad74c56544
ぬー <ryo.091979@gmail.com>
2020-01-28 16:20:19 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function () {
 
   function buildHTML(message) {
-    console.log(message)
    var image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
 
     var html = `<div class="message" data-message-id="${message.id}"> 
